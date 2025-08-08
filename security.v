39835049bef3de52b8e907088ce84f3a9d364563
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-08-08 14:58:53 +0530

    Update security.v

--- security.v
+++ security.v
@@ -1,25 +1,30 @@
 module security(
     input [31:0] data_in,
     input encryption_on,
-    input [15:0] key_access,
+    input [15:0] key_access_mem,
+    input [15:0] key_access_reg,
     output reg [31:0] data_out
 );
 reg [31:0] dataa;
 always @(*) begin
-    if (key_access == 16'h0032) begin
+    if (key_access_mem == 16'h0032) begin
         if (encryption_on) begin
             dataa = data_in - 3;
             dataa = dataa ^ 2;
             dataa = dataa + 9;
             dataa = dataa * 3;
         end else begin
+            dataa = data_in;
+        end
+    end else if (key_access_reg == 16'h0032) begin
+        if (!encryption_on) begin
             dataa = data_in / 3;
             dataa = dataa - 9;
             dataa = ~(dataa ^ 2);
             dataa = dataa + 3;
+        end else begin
+            dataa = data_in;
         end
-    end else begin
-        dataa = data_in;
     end
     data_out = dataa;
 end
