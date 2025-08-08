52dd92510b4431ac62d1b0774844f2a0808d4762
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-08-08 14:53:34 +0530

    Update security.v

--- security.v
+++ security.v
@@ -1,20 +1,26 @@
-module security(input [31:0]data_in, input encryption_on, input [15:0]key_access, output [31:0]data_out);
-wire dataa;
-if(key_access == 16'h32)
-begin 
-if(encryption_on)
-begin 
-dataa = data_in - 3;
-dataa = dataa ^ 2;
-dataa = dataa +9;
-dataa = dataa *3;
+module security(
+    input [31:0] data_in,
+    input encryption_on,
+    input [15:0] key_access,
+    output reg [31:0] data_out
+);
+reg [31:0] dataa;
+always @(*) begin
+    if (key_access == 16'h0032) begin
+        if (encryption_on) begin
+            dataa = data_in - 3;
+            dataa = dataa ^ 2;
+            dataa = dataa + 9;
+            dataa = dataa * 3;
+        end else begin
+            dataa = data_in / 3;
+            dataa = dataa - 9;
+            dataa = ~(dataa ^ 2);
+            dataa = dataa + 3;
+        end
+    end else begin
+        dataa = data_in;
+    end
+    data_out = dataa;
 end
-else
-dataa= data_in /3;
-dataa= dataa-9;
-dataa = ~(dataa ^ 2);
-dataa = dataa +3;
-end
-end
-assign data_out = dataa;
 endmodule
