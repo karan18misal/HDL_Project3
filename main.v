7866ed9c5b7b0f265c2e9831b87b992f3b9e61e5
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-08-12 16:48:33 +0530

    Update main.v

--- main.v
+++ main.v
@@ -5,5 +5,7 @@ module top_main(input clk,
                 input [9:0] address_reg2,
                 input [31:0]data_in,
                 input enable,
+                input memory_input_selection
+                input reg_input_selection
                 output data_mem,
                 output data_reg);
