6f8e2617943dfe63709a162d932f56e56d0fdb0c
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-08-12 16:53:48 +0530

    Update mux2to1.v

--- mux2to1.v
+++ mux2to1.v
@@ -2,5 +2,5 @@ module mux2to1(input [31:0]a,
                input [31:0]b,
                input select, 
                output [31:0]y)
-assign y= b?(select):a;
+assign y = select ? b:a;
 endmodule
