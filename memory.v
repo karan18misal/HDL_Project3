1654ac501cd57bdca8befe1ea7148bba3d538bdf
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-08-20 10:17:15 +0530

    Update memory.v

--- memory.v
+++ memory.v
@@ -3,19 +3,17 @@ module memory (
     input [9:0] address,
     input [31:0] write_data,
     input mem_write,
-    output [31:0] read_data,
-    output [15:0]key_access
+    output reg [31:0] read_data,
+    output [15:0] key_access
 );
-    wire [31:0]data_memory;
     reg [31:0] memory [0:1023];
+
     always @(posedge clk) begin
         if (mem_write) begin
             memory[address] <= write_data;
+        end else begin
+            read_data <= memory[address];
         end
-       else begin
-            data_memory = memory[address];
-       end
     end
-    assign key_access = 16'h32;
-    assign read_data = data_memory;
+    assign key_access = 16'h0032; 
 endmodule
