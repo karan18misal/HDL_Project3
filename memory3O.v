4c35224cebd6011b5e3fa4c81318376592039a1a
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-08-25 09:24:06 +0530

    Update memory3O.v

--- memory3O.v
+++ memory3O.v
@@ -1,1 +1,12 @@
 module memory(input clk, input data_in, input read_address, input write_address, output data_out)
+    reg [31:0] memory [0:1023];
+
+    always @(posedge clk) begin
+        if (mem_write) begin
+            memory[address] <= write_data;
+        end else begin
+            read_data <= memory[address];
+        end
+    end
+    assign key_access = 16'h0032; 
+endmodule
