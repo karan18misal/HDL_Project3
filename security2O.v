8c63b7d29dac1bd7104cb659357fb13dc56b2f51
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-08-31 20:29:49 +0530

    Create security2O.v

---
+++ security2O.v
@@ -0,0 +1,27 @@
+module security(
+    input [31:0] data_in,
+    input encryption_on,
+    input [15:0] key_access_mem,
+    input [15:0] key_access_reg,
+    output reg [31:0] data_out
+);
+reg [31:0] dataa;
+always @(*) begin
+    if (key_access_mem == 16'h0032) begin
+        if (encryption_on) begin
+            dataa = (((data_in - 3) ^ 2 )+ 9) * 3;
+
+        end else begin
+            dataa = data_in;
+        end
+    end 
+    if (key_access_reg == 16'h0032) begin
+        if (!encryption_on) begin
+            dataa = ~(((data_in / 3) - 9) ^ 2 ) + 3;
+        end else begin
+            dataa = data_in;
+        end
+    end
+    data_out = dataa;
+end
+endmodule
