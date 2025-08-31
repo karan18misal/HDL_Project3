8cd3fbcf0f61c96327eca8ba8618d85ad7b12573
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-08-31 20:16:52 +0530

    Update registers2O.v

--- registers2O.v
+++ registers2O.v
@@ -2,9 +2,11 @@ module registers(
     input clk,
     input [9:0] reg1,
     input [9:0] reg2,
-    input [9:0] address,
+    input [9:0] address_mem,
+    input [9:0] address_alu,
     input [9:0] address_to_mem,
-    input [31:0] write_data,
+    input [31:0] write_data_mem,
+    input [31:0] write_data_alu,
     output reg [31:0] read_reg1,
     output reg [31:0] read_reg2,
     output reg [31:0] memory_out,
@@ -14,11 +16,11 @@ module registers(
     reg [31:0] memory [0:1023];
 
     always @(posedge clk) begin
-            memory[address] <= write_data;
+            memory[address_mem] <= write_data_mem;
+            memory[address_alu] <= write_data_alu;
             read_reg1 <= memory[reg1];
             read_reg2 <= memory[reg2];
             memory_out <= memory[address_to_mem];
-        end
     end
 
     assign key_access = 16'h0032;
