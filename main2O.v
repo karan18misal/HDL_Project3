bdf09446790bd55997c791b1f576ea866003fb92
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-09-16 21:00:06 +0530

    Update main2O.v

--- main2O.v
+++ main2O.v
@@ -24,5 +24,5 @@ module top_main(
   
   memory m0(.clk(clk), .data_in(data_in), .read_address(read_address), .write_address(write_address), .data_in_reg(data_sectomem), .read_address_reg(read_address_reg), .write_address_reg(write_address_reg), .data_out(data_out), .data_out_reg(data_memtosec), .key_access(key_access_mem));
   registers r0(.clk(clk), .reg1(reg1), .reg2(reg2), .address_mem(address_mem), .address_alu(address_alu), .address_to_mem(address_to_mem), .write_data_mem(data_sectoreg), .write_data_alu(write_data_alu), .read_reg1(read_reg1), .read_reg2(read_reg1), .memory_out(data_regtosec), .key_access(key_access_reg));
-  security(.data_in_memory(data_memtosec), .data_in_reg(data_regtosec), .key_access_mem(key_access_mem), .key_access_reg(key_access_reg), .read_address(read_address), .write_address(write_address), .data_out_mem(data_sectomem), .data_out_reg(data_sectoreg));
+  security s0(.data_in_memory(data_memtosec), .data_in_reg(data_regtosec), .key_access_mem(key_access_mem), .key_access_reg(key_access_reg), .read_address(read_address), .write_address(write_address), .data_out_mem(data_sectomem), .data_out_reg(data_sectoreg));
 endmodule
