8042e45df80c277f15efc06dbe41ee0a64c3b341
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-09-19 10:23:54 +0530

    Update security2O.v

--- security2O.v
+++ security2O.v
@@ -1,4 +1,5 @@
 module security(
+   module security(
     input [31:0] data_in_memory,
     input [31:0] data_in_reg,
     input [15:0] key_access_mem,
@@ -10,13 +11,17 @@ module security(
 );
 always @(*) begin
     if (key_access_mem == 16'h0032) begin
-        if(write_address>128)
-            data_out_mem <= (((data_in_reg - 3) ^ 2 )+ 9) * 3; 
-        else data_out_mem <= data_in_reg;
+        if (write_address > 128)
+            data_out_mem <= (((data_in_reg - 3) * (data_in_reg - 3)) + 9) * 3;
+        else
+            data_out_mem <= data_in_reg;
     end 
+
     if (key_access_reg == 16'h0032) begin
-        if(read_address>128)
-            data_out_reg <= ~(((data_in_mem / 3) - 9) ^ 2 ) + 3;      
-        else data_out_reg <= data_in_mem;
+        if (read_address > 128)
+            data_out_reg <= ~(((data_in_memory / 3 - 9) * (data_in_memory / 3 - 9)) + 3);
+        else
+            data_out_reg <= data_in_memory;
+    end
 end
 endmodule
