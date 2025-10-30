4f8fae8288cf43e9a40243f5f289066743c0b9fe
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-10-30 20:08:32 +0530

    Update security2O.v

--- security2O.v
+++ security2O.v
@@ -8,19 +8,27 @@ module security(
     output reg [31:0] data_out_mem,
     output reg [31:0] data_out_reg
 );
-always @(*) begin
-    if (key_access_mem == 16'h0032) begin
-        if (write_address > 128)
-            data_out_mem <= (((data_in_reg - 3) * (data_in_reg - 3)) + 9) * 3;
-        else
-            data_out_mem <= data_in_reg;
-    end 
 
+always @(*) begin
+    // Memory encryption logic
+    if (key_access_mem == 16'h0032) begin
+        if (write_address > 128) begin
+            // Encryption: ((data_in_reg + 15) & 127) / 3
+            data_out_mem <= ((data_in_reg + 15) & 32'd127) / 3;
+        end else begin
+            data_out_mem <= data_in_reg;
+        end
+    end
+
+    // Register decryption logic
     if (key_access_reg == 16'h0032) begin
-        if (read_address > 128)
-            data_out_reg <= ~(((data_in_memory / 3 - 9) * (data_in_memory / 3 - 9)) + 3);
-        else
+        if (read_address > 128) begin
+            // Decryption: ((data_in_memory * 3) & 127) - 15
+            data_out_reg <= ((data_in_memory * 3) & 32'd127) - 15;
+        end else begin
             data_out_reg <= data_in_memory;
+        end
     end
 end
+
 endmodule
