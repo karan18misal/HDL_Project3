e6544f123ea88221c1840259442d922c3d040a58
Karan Misal <143802033+karan18misal@users.noreply.github.com>
2025-10-30 19:54:14 +0530

    Fix mathematical operations in security2O.v

--- security2O.v
+++ security2O.v
@@ -11,14 +11,14 @@ module security(
 always @(*) begin
     if (key_access_mem == 16'h0032) begin
         if (write_address > 128)
-            data_out_mem <= (((data_in_reg - 3) ^ (data_in_reg - 3)) + 9) * 3;
+            data_out_mem <= (((data_in_reg - 3) * (data_in_reg - 3)) + 9) * 3;
         else
             data_out_mem <= data_in_reg;
     end 
 
     if (key_access_reg == 16'h0032) begin
         if (read_address > 128)
-            data_out_reg <= ~(((data_in_memory / 3 - 9) ^ (data_in_memory / 3 - 9)) + 3);
+            data_out_reg <= ~(((data_in_memory / 3 - 9) * (data_in_memory / 3 - 9)) + 3);
         else
             data_out_reg <= data_in_memory;
     end
